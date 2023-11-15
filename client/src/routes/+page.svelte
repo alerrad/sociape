6f2e200696dfb488b84bb933bfc8d089d78279d0
Zhansen <supersegizbay@gmail.com>
2023-11-15 15:49:55 +0600

    add: deps & main page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <meta
+        name="description"
+        content="Web app for placing all your links & portfolio sharing"
+    />
+</svelte:head>
+
+<section class="py-40">
+    <div class="lg:container lg:mx-auto">
+        <div class="text-center">
+            <img src="/logo.png" alt="logo" class="m-auto w-36"/>
+            <h1 class="text-7xl font-bold mt-10">Sociape</h1>
+            <p class="mt-8 text-xl">One simple link to share all your media portfolio</p>
+            <button class="btn btn-primary my-7 text-lg">Get started</button>
+            <button class="btn btn-secondary text-lg">Example profile</button>
+        </div>
+    </div>
+</section>
