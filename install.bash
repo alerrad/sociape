4f514bb9a66ebe4db49e09f03ea39702524f4389
Madiyar <madiyar03633@gmail.com>
2023-11-16 13:48:29 +0600

    fix: activate env

--- install.bash
+++ install.bash
@@ -3,6 +3,7 @@ echo "Installing dependencies for the sveltekit client..."
 cd client && npm i
 echo "Setting up virtual env for API..."
 cd ../server && python -m venv env
+source ./env/Scripts/activate
 echo "Installing packages for the Fastapi server..."
 pip install -r requirements.txt
 echo "Installation complete! Your project is ready to run!"
